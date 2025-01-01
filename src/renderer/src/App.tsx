58232d86e3151b42105cf69a9b6f10310c0a26bf
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-01 20:25:59 +0100

    Add title

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,8 +2,11 @@ import { WindowActions } from './components/WindowActions';
 
 function App() {
   return (
-    <main className={'text-font draggable bg-background h-screen'}>
+    <main
+      className={'text-font draggable bg-background h-screen px-8 py-6 flex flex-col items-start'}
+    >
       <WindowActions />
+      <h1 className={'text-2xl font-semibold'}>Ultimate Clocker</h1>
     </main>
   );
 }
