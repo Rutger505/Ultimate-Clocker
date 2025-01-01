e3605fbb3b0139a7f104e2c6c0d758af269383f0
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-01 23:11:29 +0100

    Use luxon to display days ontill last workday before the deadline.

--- src/renderer/src/components/Report/Deadline.tsx
+++ src/renderer/src/components/Report/Deadline.tsx
@@ -1,33 +1,41 @@
+import { DateTime } from 'luxon';
+
 interface Props {
   workDays: boolean[];
   deadlineDay: number;
 }
 
 export function Deadline({ workDays, deadlineDay }: Readonly<Props>) {
-  console.log(['skldf'][2]);
-
-  const today = new Date();
-  const deadline = new Date(today.getFullYear(), today.getMonth(), deadlineDay);
-
-  if (deadline < today) {
-    deadline.setMonth(deadline.getMonth() + 1);
+  if (workDays.every((day) => !day)) {
+    return <div className="text-font">Select work days</div>;
   }
 
-  /**
-   * Check if a given day is a workday
-   * @param day 0 Monday, 1 Tuesday, ..., 6 Sunday
-   */
-  const isWorkday = (day: number) => {
-    return !!workDays[day]; // Array could be shorter than 7
-  };
+  function isWorkDay(day: DateTime) {
+    return day.weekday > 0 && day.weekday < 6 && Boolean(workDays[day.weekday - 1]);
+  }
 
-  // Calculate the number of workdays until the deadline
-  const daysUntil = 0;
+  const today = DateTime.now().startOf('day');
+  let deadline = DateTime.now().set({ day: deadlineDay }).startOf('day');
+  if (deadline < today) {
+    deadline = deadline.plus({ months: 1 });
+  }
+
+  let lastWorkDay = deadline;
+  while (!isWorkDay(lastWorkDay)) {
+    lastWorkDay = lastWorkDay.minus({ days: 1 });
+  }
+
+  const daysUntilLastWorkDayBeforeDeadline = lastWorkDay.diff(today, 'days').as('days');
 
   return (
     <div className="text-font">
-      <h3>Deadline: {deadline.toLocaleDateString()}</h3>
-      <p>Working days remaining: {daysUntil}</p>
+      <p>
+        Send report{' '}
+        {daysUntilLastWorkDayBeforeDeadline > 0
+          ? `in ${daysUntilLastWorkDayBeforeDeadline} days `
+          : 'today '}
+        ({lastWorkDay.toLocaleString(DateTime.DATE_FULL)})
+      </p>
     </div>
   );
 }
